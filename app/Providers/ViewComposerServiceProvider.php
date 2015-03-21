7d31143249e5915da7afd1375d179ba0f759e859
Vidol Chalamov <vi4ovi4o96@gmail.com>
2015-03-21 18:15:31 +0200

    Muted commit

---
+++ app/Providers/ViewComposerServiceProvider.php
@@ -0,0 +1,29 @@
+<?php namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ViewComposerServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		view()->composer('shared.layout', function($view)
+        {
+        });
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+}
